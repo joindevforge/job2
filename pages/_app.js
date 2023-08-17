52d6389563a00f7d4a69a1478f6ca99a64812915
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-17 23:51:46 +0530

    add footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import '@/styles/globals.css'
 import '../public/Founders-Grotesk/stylesheet.css'
 import '../public/xcond/stylesheet.css'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
    <Header />
       <Component {...pageProps}  />
- 
+ <Footer/>
     </>
   )
 }
